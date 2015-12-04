5f83da07372b14149e8c5eb38c568afd1cbc1a4f
KrzyHonk <honkiszkrzysztof@gmail.com>
2015-12-04 17:57:28 +0100

    #15 #17  - added two unit tests with SQL injection attack  - added simple descriptions to each test in DatabaseTests

--- h2-database-api/src/test/java/DatabaseTests.java
+++ h2-database-api/src/test/java/DatabaseTests.java
@@ -7,6 +7,10 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertTrue;
 
+/*
+* This class is a colection of database functionality tests.
+* It uses dedicated H2 database called "test" - created by default in every H2 engine
+* All of the tests uses JUnit framework, but it is suggested to check effects of every test in database */
 public class DatabaseTests {
     private final H2DatabaseAccessor accessor = new H2DatabaseAccessor("sa", "", "tcp://localhost/~/test");
 
@@ -27,6 +31,10 @@ public class DatabaseTests {
     private final List<String> returnTwo = asList("3", "Roman", "Kaskader");
     private final List<String> returnThree = asList("4", "Jaroslaw", "Kulesza");
 
+    private final List<String> insertInjection = asList("Jaroslaw", "Kulesza); drop table TEST_TABLE; --");
+
+    /* Test for addTable method.
+     Creates a sample table, without putting any rows into it*/
     @Test
     public void createTableTest() {
         try {
@@ -36,6 +44,8 @@ public class DatabaseTests {
         }
     }
 
+    /* This test creates a sample table TEST_TABLE and initializes it with some data
+     Since H2 is case sensitive to table names, we added three different test cases, for every case type (upper, lower, mixed) */
     @Test
     public void createTableAndInsertValuesUpperCaseTest() {
         try {
@@ -51,6 +61,8 @@ public class DatabaseTests {
         }
     }
 
+    /* This test creates a sample table test_table and initializes it with some data
+     Since H2 is case sensitive to table names, we added three different test cases, for every case type (upper, lower, mixed) */
     @Test
     public void createTableAndInsertValuesLowerCaseTest() {
         try {
@@ -66,6 +78,8 @@ public class DatabaseTests {
         }
     }
 
+    /* This test creates a sample table Test_Table and initializes it with some data
+     Since H2 is case sensitive to table names, we added three different test cases, for every case type (upper, lower, mixed) */
     @Test
     public void createTableAndInsertValuesMixedCaseTest() {
         try {
@@ -81,26 +95,13 @@ public class DatabaseTests {
         }
     }
 
-    @Test
-    public void createTableAndInsertValuesTestTwo() {
-        try {
-            assertTrue(accessor.addTable(TABLE_NAME_UPPER_CASE, columnNames));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            assertTrue(accessor.addRowsToTable(TABLE_NAME_UPPER_CASE, columnNames, asList(valuesOne, valuesTwo, valuesThree)));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /* Test for inserting a data in non-existing table. Proper exception should be reported */
     @Test(expected = SQLException.class)
     public void nonExistingTableTest() throws SQLException {
         accessor.addRowsToTable(NON_EXISTING_TABLE_NAME, columnNames, asList(valuesOne));
     }
 
+    /* Unit test for selectValuesFromTable method, without additional WHERE clauses */
     @Test
     public void selectFromTableTest() {
         createTableAndInsertValuesUpperCaseTest();
@@ -117,6 +118,7 @@ public class DatabaseTests {
         }
     }
 
+    /* Unit test for selectValuesFromTable method, with additional WHERE clauses */
     @Test
     public void selectFromTableWithRegexTest() {
         createTableAndInsertValuesUpperCaseTest();
@@ -138,6 +140,7 @@ public class DatabaseTests {
         }
     }
 
+    /* Unit test for countValuesFromTable method, with additional WHERE clauses */
     @Test
     public void countFromTableWithRegexTest() {
         createTableAndInsertValuesUpperCaseTest();
@@ -156,12 +159,48 @@ public class DatabaseTests {
         }
     }
 
+    /* Unit test for getColumnNames method, with additional WHERE clauses */
     @Test
     public void getColumnNames() {
         createTableAndInsertValuesUpperCaseTest();
         try {
             List<String> result = accessor.getColumnNames(TABLE_NAME_UPPER_CASE);
             assertTrue(result.containsAll(allColumnNames));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Case of SQL injection, through INSERT statement.
+     Injected query shouldn't be executed but added to table */
+    @Test
+    public void insertSqlInjectionTest() {
+        try {
+            assertTrue(accessor.addTable(TABLE_NAME_UPPER_CASE, columnNames));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            accessor.addRowsToTable(TABLE_NAME_UPPER_CASE, columnNames, asList(insertInjection));
+            List<List<String>> result = accessor.selectValuesFromTable(TABLE_NAME_UPPER_CASE, columnNames, null);
+            result.get(0).containsAll(asList("1", "Jaroslaw", "Kulesza); drop table TEST_TABLE; --"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Case of SQL injection, through SELECT statement.
+     Injected query shouldn't be executed but added as a part of regex in WHERE clause.
+     Therefore executing select statement won't return any results */
+    @Test
+    public void selectSqlInjectionTest() {
+        createTableAndInsertValuesUpperCaseTest();
+        HashMap<String, String> regexMap = new HashMap<>(2);
+        regexMap.put("first_name", "^J.*$; drop table TEST_TABLE; --");
+        try {
+            List<List<String>> result = accessor.selectValuesFromTable(TABLE_NAME_UPPER_CASE, columnNames, regexMap);
+            assertTrue(result.size() == 0);
         } catch (SQLException e) {
             e.printStackTrace();
         }
